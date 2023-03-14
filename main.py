6c86cea62b2ca47066bf363cadb3d0d6dc3ff496
astoppop <65090844+astoppop@users.noreply.github.com>
2023-03-14 17:34:39 +0000

    a

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from svg.path import parse_path
+from svg.path.path import Line
+from xml.dom import minidom
 
+# read the SVG file
+doc = minidom.parse('./boat.svg')
+path_strings = [path.getAttribute('d') for path
+                in doc.getElementsByTagName('path')]
+doc.unlink()
+
+# print the line draw commands
+for path_string in path_strings:
+    path = parse_path(path_string)
+    for e in path:
+        print(e)
+        if isinstance(e, Line):
+            x0 = e.start.real
+            y0 = e.start.imag
+            x1 = e.end.real
+            y1 = e.end.imag
+            # print("(%.2f, %.2f) - (%.2f, %.2f)" % (x0, y0, x1, y1))
